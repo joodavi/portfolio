636ef2c90656e8c52aca629c4a36956845876cfd
joodavi <joodavi@live.com>
2021-05-24 18:28:35 -0300

    ProfileCard created

---
+++ src/components/ProfileCard/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles.css';
+import ProfilePhoto from '../../assets/images/photo.jpg';
+import EnvelopeSVG from '../../assets/svg/envelope-regular.svg';
+import LinkedinSVG from '../../assets/svg/linkedin-brands.svg';
+
+function ProfileCard() {
+  return (
+    <div class="card">
+      <img src= { ProfilePhoto } alt="João Davi's" />
+      <div class="information">
+        <div class="personal-information">
+          <p class="name">João Davi Santos</p>
+          <p class="occupation">Front-end developer</p>
+        </div>
+        <div class="contact">
+          <div class="email">
+            <img class="envelope-email" src= { EnvelopeSVG } alt="Email Adress" /> 
+            {/* <p>joodavi@live.com</p> */}
+          </div>
+          <div class="linkedin">
+            <img class="linkedin-icon" src= { LinkedinSVG } alt="Link Link" /> 
+            {/* <a href="https://www.linkedin.com/in/joao-davi/" rel="noreferrer" target="_blank">João Davi</a> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileCard;
